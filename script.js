12b8c69fc1bc7cb13cff90e60ee765a10ea65bcb
piero-ac <pacoronado01@gmail.com>
2022-07-04 10:04:15 -0400

    Add query selectors for buttons
    - Add event listeners for all of the buttons

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-const numberButtons = document.getElementsByClassName("number-button");
-const operatorButtons = document.getElementsByClassName("operator-button");
-const clearAllButton = document.getElementsByClassName("AC-button");
-const clearEntryButton = document.getElementsByClassName("CE-button");
+const numberButtons = [...document.querySelectorAll(".number-button")];
+const operatorButtons = [...document.querySelectorAll(".operator-button")];
+const clearAllButton = document.querySelector(".AC-button");
+const clearEntryButton = document.querySelector(".CE-button");
+
+const operands = [];
+
+clearAllButton.addEventListener("click", () => console.log("clear all"));
+clearEntryButton.addEventListener("click", () => console.log("clear entry"));
+
+numberButtons.forEach((numberButton) =>
+  numberButton.addEventListener("click", (event) =>
+    console.log(event.target.value)
+  )
+);
+
+operatorButtons.forEach((operatorButton) =>
+  operatorButton.addEventListener("click", (event) =>
+    console.log(event.target.value)
+  )
+);
