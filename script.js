07b6a0b7d28618f9fdee2ae7160ec98f9863556a
piero-ac <pacoronado01@gmail.com>
2022-07-04 09:33:37 -0400

    Create script file and add button queries

---
+++ script.js
@@ -0,0 +1,4 @@
+const numberButtons = document.getElementsByClassName("number-button");
+const operatorButtons = document.getElementsByClassName("operator-button");
+const clearAllButton = document.getElementsByClassName("AC-button");
+const clearEntryButton = document.getElementsByClassName("CE-button");
