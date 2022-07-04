cc8127294a7826aa22947b96aa4e0efda0581fae
piero-ac <pacoronado01@gmail.com>
2022-07-04 10:43:38 -0400

    Update number btns event listener callback function
    - Create a variable to store the input of the user
    - Continue adding numbers to the input until the length of the input is <= 8
    - Update the display value as the user inputs numbers
    - Clear the input value when the AC button is pressed

--- script.js
+++ script.js
@@ -5,19 +5,25 @@ const clearAllButton = document.querySelector(".AC-button");
 const clearEntryButton = document.querySelector(".CE-button");
 
 const operands = [];
+let input = "";
 
 clearAllButton.addEventListener("click", () => {
   operands.splice(0, operands.length);
   display.textContent = "0";
+  input = "";
   //console.log("clear all");
 });
 
 clearEntryButton.addEventListener("click", () => console.log("clear entry"));
 
 numberButtons.forEach((numberButton) =>
-  numberButton.addEventListener("click", (event) =>
-    console.log(event.target.value)
-  )
+  numberButton.addEventListener("click", (event) => {
+    if (input.length <= 8) {
+      input += event.target.value;
+      display.textContent = input;
+    }
+    //console.log(event.target.value)
+  })
 );
 
 operatorButtons.forEach((operatorButton) =>
